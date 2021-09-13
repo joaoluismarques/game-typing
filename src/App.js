e2a49aaa0def5ed5f02fc71066d2db97f13677d1
joaoluismarques <joaolu19@outlook.com>
2021-09-13 15:10:55 -0300

    Initial structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <div className="container">
+            <div className="valid-keys">
+                <span className="matched">jo</span>
+                <span className="remainder">ao</span>
+            </div>
+            <div className="typed-keys">asfjo</div>
+            <div className="completed-words">
+                <ol>
+                    <li>foco</li>
+                    <li>esforço</li>
+                    <li>determinação</li>
+                    <li>dedicação</li>
+                    <li>liderança</li>
+                </ol>
+            </div>
+        </div>
+    );
+};
 
 export default App;
