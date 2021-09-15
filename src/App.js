d9868646fabb4afbdf89949e127e4586e507483d
joaoluismarques <joaolu19@outlook.com>
2021-09-15 13:17:43 -0300

    Typed Keys

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const App = () => {
+    const [typedKeys, setTypedKeys] = useState([]);
+
+    const handleKeyDown = (e) => {
+        e.preventDefault();
+        const { key } = e;
+
+        setTypedKeys((prevTypedKeys) => {
+            return [...prevTypedKeys, key];
+        });
+
+        console.log('key', key);
+    };
+
+
     return (
-        <div className="container">
+        <div className="container" tabIndex="0" onKeyDown={handleKeyDown}>
             <div className="valid-keys">
                 <span className="matched">jo</span>
                 <span className="remainder">ao</span>
             </div>
-            <div className="typed-keys">asfjo</div>
+            <div className="typed-keys">{typedKeys ? typedKeys.join(' ') : null}</div>
             <div className="completed-words">
                 <ol>
                     <li>foco</li>
